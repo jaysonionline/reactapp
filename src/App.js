1fffff9e6752431eaf61b6222f0e1d9d11da27f9
techtechgoofficial <engagewithknowledge@gmail.com>
2024-08-20 23:05:52 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,82 @@
-import logo from './logo.svg';
+
+import { useState } from 'react';
+import React from 'react';
+
 import './App.css';
 
+import Header from "./Header"
+
+
+class MyCar extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      firstName: "Nikhil",
+      age: 37
+    }
+
+  }
+
+  changeAge = () =>{
+    this.setState({age: 40})
+  }
+
+  render() {
+    return (
+      <div>
+        <hr></hr>
+        <h2>Child Component</h2>
+        <div>
+          car color is : {this.props.cars.color}
+        </div>
+        <div>My name is {this.state.firstName} and age {this.state.age}</div>
+        <button onClick={this.changeAge}>Change age</button>
+
+      </div>
+    )
+  }
+
+}
+
+
+
+
 function App() {
+
+  // let count = 0;
+
+  // const car = {color:"red", company:"ford"};
+
+  const [car, setCar] = useState({ color: "red", company: "ford" })
+
+  const [count, setCount] = useState(0);
+
+  const incrementCounter = () => {
+    // count = count + 1;
+
+    setCount((count) => count + 1)
+
+  }
+
+  const changeColor = () => {
+
+    setCar({ color: "blue", company: "ford" })
+
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+
+      <h1> Update Counter </h1>
+
+      <h2> Counter: {count}</h2>
+
+      <button onClick={incrementCounter}>Increment</button>
+      <button onClick={changeColor}>Change Color</button>
+
+      <MyCar cars={car}></MyCar>
+
     </div>
   );
 }
---
+++ src/Header.js
@@ -0,0 +1,9 @@
+import React from "react";
+function Header(props) {
+    return (
+        <div>
+            <h1>{props.department}</h1>;
+        </div>
+    );
+}
+export default Header;
